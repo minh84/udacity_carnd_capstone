e5e475ccf6d02e1095548add3677993be40ba2a5
Minh VU PC <vungocminh84@gmail.com>
2018-07-20 22:39:44 +0100

    fix bug in velocity_cb

--- CarND-Capstone/ros/src/twist_controller/dbw_node.py
+++ CarND-Capstone/ros/src/twist_controller/dbw_node.py
@@ -104,7 +104,7 @@ class DBWNode(object):
         self.angular_vel = msg.twist.angular.z
 
     def velocity_cb(self, msg):
-        self.current_vel = msg
+        self.current_vel = msg.twist.linear.x
 
     def publish(self, throttle, brake, steer):
         tcmd = ThrottleCmd()
